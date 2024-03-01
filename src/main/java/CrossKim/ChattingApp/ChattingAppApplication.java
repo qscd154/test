2da82995df5298328c4e7050345335ee8ef866af
qscd154 <qscd154@gmail.com>
2024-03-01 21:27:16 +0900

    First Commit

---
+++ src/main/java/CrossKim/ChattingApp/ChattingAppApplication.java
@@ -0,0 +1,13 @@
+package CrossKim.ChattingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CrossKim/ChattingApp/ChattingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package CrossKim.ChattingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
